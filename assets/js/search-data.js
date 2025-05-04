25d270a6f2dadaec6f10a53b862c79eeaca5bd46
pilsHan <87313780+pilsHan@users.noreply.github.com>
2025-05-04 10:17:51 +0000

    Deploying to gh-pages from @ pilsHan/pilsHan.github.io@271ba5bfa510d3f86cc0bb138d11ba633179fcc6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,14 +48,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6A%69%73%75%33%37%30%39@%61%6A%6F%75.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-rss',
@@ -69,14 +62,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=kCVEGswAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
